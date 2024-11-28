920990d0ca807f6c98401e96d8497b8aada2c678
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-11-28 08:07:27 +0530

    src: added changes with showSnippet and writeSnippet

--- main.go
+++ main.go
@@ -1,18 +1,47 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // handler for showSnippet
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet ... "))
+	// w.Write([]byte("Display a specific snippet ... "))
+
+	//extract the value of the id parameter from the query string and try to convert to a string
+	// using strconv.Atoi(). if it can't be converted to an integer, or the value is < 1, we return a 404 not found response
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	//using fmt.Fprintf() function to interpolate the id value with our response and write it http.ResponseWriter
+	fmt.Fprintf(w, "Display a specific snippet with ID %d ...", id)
 }
 
 // handler for createSnippet
 func createSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Create a new snippet ... "))
+	// w.Write([]byte("Create a new snippet ... "))
+	// send a 405 (method not alowed) HTTP status code unless the request method is POST.
+	// and also set the header to Allow: Post header , to let the user know which request methods are supported for that
+	// particular URL
+
+	if r.Method != "POST" {
+		// w.Header().Set("Allow", "Post")
+		// w.WriteHeader(405)
+		// w.Write([]byte("Method Not Allowed"))
+		// return
+		// instead of this much hard work , we can directly send a 405 status code and "Method Allowed" string as the response body
+		w.Header().Set("Allow", "POST")
+		http.Error(w, "Method Not Allowed", 405)
+		return
+	}
+
+	w.Write([]byte("Create a new snippet..."))
 }
 
 // handler function which writes a byte slice containing "hello from snippetbox" as the response body
