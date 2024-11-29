3268f697f97b481f75fae51c44953ebd35bf5b03
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-11-29 19:22:54 +0530

    src: added assets

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,6 +11,15 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	//craete a file server which will server file  out of the "./ui/static" directory
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	//using mux.Handle() to register the file server as the handler all URL paths that start with "/static/"
+	// for matching paths, we strip "/static" prefix
+
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
---
+++ docs/fileserver-endpoints.md
@@ -0,0 +1,5 @@
+/ -> home -> display the home page 
+/snippet?id=1 -> showSnippet -> display a specific snippet 
+/snippet/create -> createSnippet -> create a new snippet 
+/static/ -> http.FileServer -> serve a specific static file 
+
