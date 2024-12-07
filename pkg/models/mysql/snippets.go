a987fee5710f893e377d758877e756d0c9c34815
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-12-07 23:27:52 +0530

    src: added insert model

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -13,7 +13,28 @@ type SnippetModel struct {
 
 // insert a new snippet into the databse
 func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
-	return 0, nil
+	//write the sql statement we want to execute.
+	// split over 2 lines for readability
+	stmt := `INSERT INTO snippets (title,content,created,expires)
+	VALUES(?,?,UTC_TIMESTAMP(),DATE_ADD(UTC_TIMESTAMP(),INTERVAL ? DAY))`
+
+	//exec() method on embedded connection pool to execute the stmt
+
+	result, err := m.DB.Exec(stmt, title, content, expires)
+
+	if err != nil {
+		return 0, err
+	}
+
+	//use the LastInsertId() method on result object to get the ID our newly inserted
+	// record in snippets table
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
 }
 
 // return a specfici snippet based on its id
