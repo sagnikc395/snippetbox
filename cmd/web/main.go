9fe19158615f466364c8f4bb1051bc07535ac4cb
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-12-07 17:40:49 +0530

    src: refactor endpoints to routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -39,36 +39,13 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
-
-	//create a file server which will server file  out of the "./ui/static" directory
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	//using mux.Handle() to register the file server as the handler all URL paths that start with "/static/"
-	// for matching paths, we strip "/static" prefix
-
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	// log.Println("Starting server on %s", addr)
-	//print messages using our own logger
-
-	//init a new http.Server struct. we are setting the addr and handler fields that the server uses the same network address
-	// and routes as before and the errorlog field so that the server now uses the custom errorlog logger for
-	// sendinf the event of any problems.
-
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
 	infoLog.Printf("starting server on %s", *addr)
-	// err := http.ListenAndServe(*addr, mux)
-	// use new logger
 	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
