d05daa332695c8f48a371ca595dbcfedf10bde11
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-12-08 00:09:33 +0530

    src: added get method

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -39,7 +39,21 @@ func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
 
 // return a specfici snippet based on its id
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
-	return nil, nil
+	stmt := `SELECT id,title,content,created,expires from snippets where expires > UTC_TIMESTAMP() and id = ?`
+
+	row := m.DB.QueryRow(stmt, id)
+
+	s := &models.Snippet{}
+
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+
+	if err == sql.ErrNoRows {
+		return nil, models.ErrNoRecord
+	} else if err != nil {
+		return nil, err
+	}
+
+	return s, nil
 }
 
 // retrun the 10 most recently created snippets
